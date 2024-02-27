e980a3fe96bea28e6f39ccc475fedd5703758641
krish858 <unknowmortal57@gmail.com>
2024-02-27 23:47:36 +0530

    Create 204. Count Prime.java
    just making logic lol ;)
    (will use binary search)

---
+++ 204. Count Prime.java
@@ -0,0 +1,18 @@
+ 
+class Main{
+    public static  void Function(int n){
+        int start = 0;
+        int end = n;
+        boolean a = false;
+        int count;
+        for (int i = 2; i < end; i++) {
+            for (int j = 2; j < ; j++) {
+
+            }
+
+        }
+    }
+    public static void main(String[] args) {
+        Function(10);
+    }
+}
